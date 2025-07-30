9e38b3b8a79f99fcc1d80f4635116fecbef1d36c
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-30 22:35:09 +0600

    js-array

---
+++ array-index.js
@@ -0,0 +1,13 @@
+const numbers = [9, 15, 48, 18, 23, 25, 89, 23, 1,];
+console.log (numbers);
+
+// get element by index value
+
+console.log (numbers[2]);
+const fourth = numbers[3];
+console.log (fourth);
+
+// set or update element value by index
+
+numbers[1] = 9999;
+console.log (numbers);
---
+++ array-length.js
@@ -0,0 +1,2 @@
+const numbers = [9, 15, 48, 18, 23, 25, 89, 23, 1,];
+console.log (numbers.length);
