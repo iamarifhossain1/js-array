c5928d466ab8d35f69b95ad63400c4ad27ea72d8
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-08 00:52:45 +0600

    array-looping

---
+++ array-looping.js
@@ -0,0 +1,33 @@
+let fruits = ['Apple', 'Orange', 'Watermelon', 'Coconut', 'Taal', 'Amra'];
+// console.log(fruits[0]);
+
+
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(i);
+    console.log(fruits[i]);   
+}
+
+let numbers = [22, 42, 58, 64, 29, 43,];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+    
+}
+
+
+let foool = ['Apple', 'Orange', 'Watermelon', 'Coconut', 'Taal', 'Amra'];
+let n = 0;
+
+while (n < foool.length) {
+    console.log(foool[n]);
+    n++
+}
+
+const prices = [20, 30, 40, 50, 60, 70, 80, 90, 100];
+let price = 0;
+
+while (price < prices.length) {
+    console.log(prices[price]);
+    price++;
+    
+}
