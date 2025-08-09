ce550a3cca3eb76ada5f31fb6064811140524001
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-08-10 03:01:37 +0600

    array-task

--- array-task.js
+++ array-task.js
@@ -27,4 +27,71 @@ for (const number of numbers) {
 
 console.log(num);
 
+/** Task - 3
+ * Use a for...of loop to concatenate all the elements of an array into a single string.
+ */
 
+var names = ['Tom', 'Tim', 'Tin', 'Tik'];
+let storeArray = [];
+
+for (const name of names) {
+    storeArray.push(name);
+}
+
+console.log(storeArray.join(""));
+
+
+/** Task - 4
+ * Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+ */
+
+const statement = 'I am a hard working person';
+let str = statement.split(' ');
+let rev = [];
+for (const words of str) { 
+    rev.unshift(words);
+}
+
+console.log(rev.join(' '));
+
+/** Task - 5
+ * Copy the given array into another array so that changing the copy does not affect the original.Change the first element of the copied array to 99.
+ */
+
+const nums = [1, 2, 3];
+const newNums = [];
+newNums.push(99, 2, 3);
+console.log(nums);
+console.log(newNums);
+
+
+/** Task - 6
+ * Given an array of student objects, print each student’s name and marks.
+ */
+
+const students = [
+    {Name: 'John', Marks: 85},
+    {Name: 'Alice', Marks: 90}
+]
+
+for (const student of students) {
+    const newStudent = student;
+    const studentInfo = 'Name: ' + newStudent.Name + ', ' + 'Marks: ' + newStudent.Marks;
+    console.log(studentInfo);
+    
+    
+}
+
+
+/** Task - 7
+ *  Given a 2D array, update the value at second row first item to 99 and print the updated array.
+ */
+
+let values = [
+     [1, 2],
+     [3, 4],
+     [5, 6]
+]
+
+values[1][0] = 99;
+console.log(values);
